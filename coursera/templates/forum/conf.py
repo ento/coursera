b112f235f6a8b144bc66200981a69b27ac1d4ac3
Marica Odagaki <ento.entotto@gmail.com>
2014-04-06 22:58:40 +0900

    Remove year from copyright string

--- coursera/templates/forum/conf.py
+++ coursera/templates/forum/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'{{ class_name }}'
-copyright = u'2014, Courserians'
+copyright = u'Courserians'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
